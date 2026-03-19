fcd81e7556ba98d846666cbfa2ed928e2ef8cc82
Anna <annapet200313@yandex.ru>
2026-03-19 19:11:37 +0800

    Feat/app layout (#2)
    * feat: initial project setup with types and add Button, ProgressBar components
    
    * feat:I have modified and improved the project components, and added new types and constants.
    
    * feat: craete layout for the dashbord, add sidebar with components, add sheet and new hooks

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+
+export default function RootPage() {
+  // потом здесь будет проверка сессии Supabase
+  // если залогинен → /dashboard
+  // если нет → /login
+  
+  // пока просто редиректим на dashboard
+  redirect("/dashboard");
+}
